881b807a1c4225e8348a305160a28986290111d0
Letizia Obiglio <letizia.obiglio@gmail.com>
2020-02-20 17:23:58 +0100

    feat: status online dynamique

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import './Contact.css'
 
-function Contact(props) {
-    return (
-        <div className="Contact">
-            <img src={props.avatar} className="avatar" />
-            <div>
-                <h4 className="name"> {props.name} </h4>
-                <p className="status status-text">
-                    <span className={props.online ? 'status-online' : 'status-offline'}></span>
-                    {props.online ? 'online' : 'offline'}
-                </p>
-            </div>
+class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { online: this.props.online }
+    }
+    render() { 
+        return (  <div className="Contact">
+        <img src={this.props.avatar} className="avatar" alt={this.props.name} />
+        <div>
+            <h4 className="name"> {this.props.name} </h4>
+            <p className="status status-text" onClick={ event => {
+                const newline = !this.state.online;
+                this.setState({online: newline})
+            }} >
+                <span className={this.state.online ? 'status-online' : 'status-offline'}></span>
+                {this.state.online ? 'online' : 'offline'}
+            </p>
         </div>
-    )
+    </div> );
+    }
 }
-
+ 
 export default Contact;
